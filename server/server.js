e3cf7aca077db7fe067e0aba3e830402a7810c8c
Antonio Rivaldo21 <rival2silva365@gmail.com>
2025-11-13 20:57:51 -0300

    teste de conexão
    ok.

---
+++ server/server.js
@@ -0,0 +1,11 @@
+import { createServer } from 'node:http';
+
+
+const server = createServer((req, res) => {
+    res.writeHead(200, {'content-type': 'text/plain'});
+    res.end('Servidor Node.js funcionando!');
+
+});
+
+server.listen(3000, ()=> console.log('Servidor rodando na porta 3000'));
+export default App
